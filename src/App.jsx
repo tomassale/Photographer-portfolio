8811eaed7cad71b063bc90e11ee38f4150ebaf8e
Tomás Sale <tomassale@hotmail.com>
2024-01-21 21:24:44 -0300

    Files added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from './component/footer/Footer'
+import Navbar from './component/header/Navbar'
+
+const App = () => {
+  return(
+    <>
+      <Navbar/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
