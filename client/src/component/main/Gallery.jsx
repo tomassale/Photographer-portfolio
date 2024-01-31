d26eae4c4c3559e2f9d52627471acd8098b9bbe8
Tomás Sale <tomassale@hotmail.com>
2024-01-31 16:35:41 -0300

    Modification on Gallery

--- client/src/component/main/Gallery.jsx
+++ client/src/component/main/Gallery.jsx
@@ -1,22 +1,19 @@
 import Initial from './section/Initial'
+import Loader from './util/Loader'
 import Artist from './section/ItemGallery'
+import data from '../../data/artist.json'
 
 const Gallery = () => {
   return (
     <div className='gallery'>
       <Initial/>
-      <Artist name='cosquin' title='cosquin rock' n1='1' n2='2' n3='3'/>
-      <Artist name='kapanga' title='kapanga' n1='1' n2='6' n3='7'/>
-      <Artist name='pericos' title='los pericos' n1='1' n2='5' n3='7'/>
-      <Artist name='renga' title='la renga' n1='4' n2='6' n3='7'/>
-      <Artist name='rataBlanca' title='rata blanca' n1='2' n2='3' n3='8'/>
-      <Artist name='skay' title='skay beilinson' n1='3' n2='4' n3='7'/>
-      <Artist name='koinoYokan' title='koino yokan' n1='1' n2='2' n3='4'/>
-      <Artist name='babasonicos' title='babasonicos' n1='1' n2='2' n3='6'/>
-      <Artist name='pastillasDelAbuelo' title='las pastillas del abuelo' n1='1' n2='2' n3='3'/>
-      <Artist name='asspera' title='asspera' n1='1' n2='3' n3='4'/>
-      <Artist name='parraleños' title='parraleños' n1='1' n2='2' n3='3'/>
-      <Artist name='ntvg' title='no te va gustar' n1='2' n2='6' n3='9'/>
+      {!data ?(
+        <Loader/>
+      ):(
+        data.map((obj) => {
+          return <Artist key={obj.id} art={obj}/>
+        })
+      )}
     </div>
   )
 }
--- client/src/component/main/section/ItemGallery.jsx
+++ client/src/component/main/section/ItemGallery.jsx
@@ -1,13 +1,17 @@
-const ItemGallery = ({name, title, n1, n2, n3}) => {
-  const upper = title[0].toUpperCase() + title.slice(1)
-  const imageSrc = (index) => `./img/${name}/img${index}.jpg`;
+import { NavLink } from 'react-router-dom'
+
+const ItemGallery = ({art}) => {
+  const upper = art.title[0].toUpperCase() + art.title.slice(1)
+  const imageSrc = (index) => `./img/${art.name}/img${index}.jpg`;
 
   return (
-    <div id={name} className='itemGallery'>
-      <h1>{title.toUpperCase()}</h1>
-      <div id={`${name}Image`}>
-        {[n1, n2, n3].map((index) => (
-          <img key={index} src={imageSrc(index)} alt={`Imagen ${index} ${upper}`} />
+    <div id={art.name} className='itemGallery'>
+      <h1>{art.title.toUpperCase()}</h1>
+      <div id={`${art.name}Image`}>
+        {[art.n1, art.n2, art.n3].map((index) => (
+          <NavLink to={`/Gallery/${art.id}`}>
+            <img key={index} src={imageSrc(index)} alt={`Imagen ${index} ${upper}`} />
+          </NavLink>
         ))}
       </div>
     </div>
